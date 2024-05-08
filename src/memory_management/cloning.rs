ed88d9026b0d261a10fd48f8fe8bb5bc1d473c85
AchroDev <ztblaine@protonmail.com>
2024-05-08 01:50:53 -0500

    Added code and comments to cloning.rs

--- src/memory_management/cloning.rs
+++ src/memory_management/cloning.rs
@@ -1,7 +1,42 @@
 /*
-
+   Cloning Avoids Moves
 */
 
-//
+/*
+*   One way to avoid moving data is to clone it using the '.clone()' method. For example,
+*   we can fix the safety issue in the [Moving_Variables] program with a clone:
+*/
 
-fn main() {}
+fn main() {
+    let first = String::from("Ferris");
+    let first_clone = first.clone();
+    let full = add_suffix(first_clone);
+    println!("{} originally, {}", full, first);
+}
+
+fn add_suffix(mut name: String) -> String {
+    name.push_str(" Jr.");
+    name
+}
+
+/*
+// L1           Heap
+Stack
+main
+first	●   ->  Ferris
+first_clone	● -> Ferris
+
+// L2
+Stack           Heap
+main
+first	●   ->  Ferris
+first_clone	⦻  // We are cloning and don't need to add anything to the heap
+full	●   ->  Ferris
+*/
+
+/*
+*   Observe that at L1, 'first_clone' did not "shallow" copy the pointer in 'first', but
+*   isntead "deep" copied the string data into a new heap allocation. Therefore at L2,
+*   while 'first_clone' has been moved and invalidated by 'add_suffix', the original 'first'
+*   variable is unchanged. It is safe to continue using 'first'.
+*/
